502999b031d2609e21e02ac9432d8b736e485940
t4t5u0 <ymmtryk0902@gmail.com>
2020-04-11 05:49:44 +0900

    ステータス確認用のコマンド、チェックを実装

--- prompt.py
+++ prompt.py
@@ -32,15 +32,17 @@ class Command(Cmd):
         else:
             if len(char) == 1:
                 self.current_character = char[0]
-            conn = sqlite3.connect('./db/character_list.db', detect_types=sqlite3.PARSE_DECLTYPES)
+            conn = sqlite3.connect(
+                './db/character_list.db', detect_types=sqlite3.PARSE_DECLTYPES)
             c = conn.cursor()
             c.execute(
-            '''CREATE TABLE IF NOT EXISTS character_list(
+                '''CREATE TABLE IF NOT EXISTS character_list(
                 id     integer primary key,
                 name   text);
             ''')
             for item in char:
-                c.execute('INSERT INTO character_list (name) VALUES (?)', (item,))
+                c.execute(
+                    'INSERT INTO character_list (name) VALUES (?)', (item,))
                 print(f'append "{item}"')
             conn.commit()
 
@@ -55,12 +57,30 @@ class Command(Cmd):
         pass
 
     def do_check(self, inp):
-        conn = sqlite3.connect('./db/character_list.db', detect_types=sqlite3.PARSE_DECLTYPES)
-        #conn = sqlite3.connect(':memory:', detect_types=sqlite3.PARSE_DECLTYPES)
-        c = conn.cursor()
-        for row in c.execute('SELECT skill_name, skill_effect, round FROM character_list WHERE chara_name == ?;', (inp,)):
-            print(row)
-        conn.close()
+        '''ステータス確認用のコマンド\ncheck [character] ex: check ギルバート'''
+        char = inp.split()
+        if len(char) == 0:
+            print('引数が少なすぎます。check は引数を1つとります。詳細は help check で確認してください。')
+        elif len(char) >= 2:
+            print('引数が多すぎます。check は引数を1つとります。詳細は help check で確認してください。')
+        else:
+            char = char[0]
+            conn = sqlite3.connect('./db/status_list.db',
+                                   detect_types=sqlite3.PARSE_DECLTYPES)
+            c = conn.cursor()
+            # c.execute('''CREATE TABLE IF NOT EXISTS status_list(
+            #     id              integer primary key,
+            #     chara_name      text,
+            #     skill_name      text,
+            #     skill_effect    text,
+            #     round           integer);
+            # ''')
+            # conn.commit()
+            print(char)
+            for row in c.execute('SELECT skill_name, skill_effect, round FROM　status_list WHERE chara_name == ?;', (char,)):
+                print(
+                    f'skill name:{row[0]:10} skill effect:{row[1]:10} round:{row[2]:10}')
+            conn.close()
 
     def do_start(self):
         pass
@@ -94,27 +114,27 @@ class Command(Cmd):
 
     def do_helps(self, inp):
         print(
-    f'''コマンド一覧を表示
-    {'─'*100}
-    {'name':^10}| {'explanation':^40}| {'arguments':^50}
-    {'─'*100}
-    {'append':<10}| キャラクタを追加{' '*(40-subcommands.get_east_asian_count('キャラクタを追加'))}| キャラクタ1 キャラクタ2 キャラクタ3 ...{' '*(50-subcommands.get_east_asian_count('キャラクタ1 キャラクタ2 キャラクタ3 ...'))}
-    {'change':<10}| 状態を変更したいキャラクタを変更{' '*(40-subcommands.get_east_asian_count('状態を変更したいキャラクタを変更'))}| キャラクタ/キャラクタID{' '*(50-subcommands.get_east_asian_count('キャラクタ/キャラクタID'))}
-    {'ls':<10}| キャラクタ一覧を表示{' '*(40-subcommands.get_east_asian_count('キャラクタ一覧を表示'))}| 引数なし{' '*(50-subcommands.get_east_asian_count('引数なし'))}
-    {'kill':<10}| キャラクタを消去{' '*(40-subcommands.get_east_asian_count('キャラクタを消去'))}| キャラクタ/キャラクタID{' '*(50-subcommands.get_east_asian_count('キャラクタ/キャラクタID'))}
-    {'add':<10}| 技能・呪文などを付与{' '*(40-subcommands.get_east_asian_count('技能・呪文などを付与'))}| 技能・呪文など 効果ラウンド上書き(オプショナル){' '*(50-subcommands.get_east_asian_count('技能・呪文など 効果ラウンド上書き(オプショナル)'))}
-    {'remove':<10}| 技能・呪文などを消去{' '*(40-subcommands.get_east_asian_count('技能・呪文などを消去'))}| 技能・呪文など/状態ID{' '*(50-subcommands.get_east_asian_count('技能・呪文など/状態ID'))}
-    {'check':<10}| 技能・呪文などの一覧を表示{' '*(40-subcommands.get_east_asian_count('技能・呪文などの一覧を表示'))}| 引数なし/キャラクタ/キャラクタID{' '*(50-subcommands.get_east_asian_count('引数なし/キャラクタ/キャラクタID'))}
-    {'start':<10}| 手番を開始{' '*(40-subcommands.get_east_asian_count('手番を開始'))}| 引数なし{' '*(50-subcommands.get_east_asian_count('引数なし'))}
-    {'end':<10}| 手番を終了{' '*(40-subcommands.get_east_asian_count('手番を終了'))}| 引数なし{' '*(50-subcommands.get_east_asian_count('引数なし'))}
-    {'neko':<10}| にゃーん{' '*(40-subcommands.get_east_asian_count('にゃーん'))}| 引数なし{' '*(50-subcommands.get_east_asian_count('引数なし'))}
-    {'helps':<10}| これ{' '*(40-subcommands.get_east_asian_count('これ'))}| 引数なし{' '*(50-subcommands.get_east_asian_count('引数なし'))}
-    {'exit':<10}| アプリケーションを終了させる{' '*(40-subcommands.get_east_asian_count('アプリケーションを終了させる'))}| 引数なし{' '*(50-subcommands.get_east_asian_count('引数なし'))}
-    {'─'*100}''')
+            f'''コマンド一覧を表示
+{'─'*100}
+{'name':^10}| {'explanation':^40}| {'arguments':^50}
+{'─'*100}
+{'append':<10}| キャラクタを追加{' '*(40-subcommands.get_east_asian_count('キャラクタを追加'))}| キャラクタ1 キャラクタ2 キャラクタ3 ...{' '*(50-subcommands.get_east_asian_count('キャラクタ1 キャラクタ2 キャラクタ3 ...'))}
+{'change':<10}| 状態を変更したいキャラクタを変更{' '*(40-subcommands.get_east_asian_count('状態を変更したいキャラクタを変更'))}| キャラクタ/キャラクタID{' '*(50-subcommands.get_east_asian_count('キャラクタ/キャラクタID'))}
+{'ls':<10}| キャラクタ一覧を表示{' '*(40-subcommands.get_east_asian_count('キャラクタ一覧を表示'))}| 引数なし{' '*(50-subcommands.get_east_asian_count('引数なし'))}
+{'kill':<10}| キャラクタを消去{' '*(40-subcommands.get_east_asian_count('キャラクタを消去'))}| キャラクタ/キャラクタID{' '*(50-subcommands.get_east_asian_count('キャラクタ/キャラクタID'))}
+{'add':<10}| 技能・呪文などを付与{' '*(40-subcommands.get_east_asian_count('技能・呪文などを付与'))}| 技能・呪文など 効果ラウンド上書き(オプショナル){' '*(50-subcommands.get_east_asian_count('技能・呪文など 効果ラウンド上書き(オプショナル)'))}
+{'remove':<10}| 技能・呪文などを消去{' '*(40-subcommands.get_east_asian_count('技能・呪文などを消去'))}| 技能・呪文など/状態ID{' '*(50-subcommands.get_east_asian_count('技能・呪文など/状態ID'))}
+{'check':<10}| 技能・呪文などの一覧を表示{' '*(40-subcommands.get_east_asian_count('技能・呪文などの一覧を表示'))}| 引数なし/キャラクタ/キャラクタID{' '*(50-subcommands.get_east_asian_count('引数なし/キャラクタ/キャラクタID'))}
+{'start':<10}| 手番を開始{' '*(40-subcommands.get_east_asian_count('手番を開始'))}| 引数なし{' '*(50-subcommands.get_east_asian_count('引数なし'))}
+{'end':<10}| 手番を終了{' '*(40-subcommands.get_east_asian_count('手番を終了'))}| 引数なし{' '*(50-subcommands.get_east_asian_count('引数なし'))}
+{'neko':<10}| にゃーん{' '*(40-subcommands.get_east_asian_count('にゃーん'))}| 引数なし{' '*(50-subcommands.get_east_asian_count('引数なし'))}
+{'helps':<10}| これ{' '*(40-subcommands.get_east_asian_count('これ'))}| 引数なし{' '*(50-subcommands.get_east_asian_count('引数なし'))}
+{'exit':<10}| アプリケーションを終了させる{' '*(40-subcommands.get_east_asian_count('アプリケーションを終了させる'))}| 引数なし{' '*(50-subcommands.get_east_asian_count('引数なし'))}
+{'─'*100}''')
 
     def do_exit(self, arg):
         x = input('終了しますか？ [Y/n] ')
-        if  x == 'Y' or x == 'y':
+        if x == 'Y' or x == 'y':
             exit(0)
         elif x == 'N' or x == 'n':
             pass
@@ -123,6 +143,7 @@ class Command(Cmd):
 
     def help_help(self):
         print('help cmd で cmd の説明を表示します')
+
 
 if __name__ == '__main__':
     Command().cmdloop()
