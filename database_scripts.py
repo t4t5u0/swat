bd0ee3af04b682cba676b9f0cc3118bde6f00b2a
t4t5u0 <--global>
2020-04-25 23:30:44 +0900

    database_scriptsが壊れていたので修正

--- database_scripts.py
+++ database_scripts.py
@@ -6,7 +6,6 @@ from swtool import database_commands
 
 def main():
     arg = sys.argv
-    flag = False
     if '--help' in arg:
         print('データベースの作成や削除を行うことができます')
         print('パラメータにはデータベースの名前を指定することで、そのデータベースを初期化して作成することができます')
@@ -22,30 +21,22 @@ def main():
         if len(arg) == 2:
             print('引数が少なすぎます。作成するデータベースファイルを選んでください')
             print('$ python database_scripts.py --help')
-            flag = True
             exit(0)
         if '--all' in arg:
             # 全部消す
             database_commands.delete_character_list()
             database_commands.delete_status_list()
             database_commands.delete_skill_list()
-            flag = True
             exit(0)
         if '--character_list' in arg:
             # キャラクタ一覧が入ってる
             database_commands.delete_character_list()
-            flag = True
         if '--status_list' in arg:
             # キャラクタ毎のステータスが入ってる
             database_commands.delete_status_list()
-            flag = True
         if '--skill_list' in arg:
             # ステータス変動がある技能一覧や効果の一覧が入ってる
             database_commands.delete_skill_list()
-            flag = True
-        if not flag:
-            print('不正なオプションです。--help で確認してください')
-
 
     # ファイル生成系
     else:
@@ -58,17 +49,12 @@ def main():
         if '--character_list' in arg:
             # キャラクタ一覧が入ってる
             database_commands.create_character_list()
-            flag = True
         if '--status_list' in arg:
             # キャラクタ毎のステータスが入ってる
             database_commands.create_status_list()
-            flag = True
         if '--skill_list' in arg:
             # ステータス変動がある技能一覧や効果の一覧が入ってる
             database_commands.create_skill_list()
-            flag = True
-        if not flag:
-
 
 
 if __name__ == '__main__':
