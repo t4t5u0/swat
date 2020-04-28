9d7c0ba2830b301f7c567f8587ed6ab31019e733
t4t5u0 <ymmtryk0902@gmail.com>
2020-04-28 16:12:28 +0900

    とりあえず技能を削除できるようになった

--- prompt.py
+++ prompt.py
@@ -226,8 +226,44 @@ class Command(Cmd):
                             conn.commit()
                         print(f'{skill_name} to {self.current_character}')
 
-    def do_remove(self):
-        pass
+    def do_rm(self, inp):
+        '''追従しているキャラの技能を削除するコマンド, 一度に複数消去可'''
+        if self.current_character == '':
+            print('対象にするキャラクタを設定してください')
+        else:
+            arg = inp.split()
+            conn = sqlite3.connect('./db/data.db')
+            c = conn.cursor()
+            for item in arg:
+                # skill_list の skill_name をLIKE検索する
+                # 同名で複数効果を持っているものがあるから、それに対応する(ヘイストなど)
+                c.execute('SELECT skill_name FROM status_list WHERE chara_name = ? AND skill_name LIKE ?',(self.current_character, f'%{item}%'))
+                # fetchall するとタプルのリストで返ってくる
+                skill_names = c.fetchall()
+                print(skill_names)
+                if len(skill_names) > 1:
+                    # 検索して複数見つかった場合の処理
+                    for i, skill_name in enumerate(skill_names):
+                        #skill_name = skill_name[0]
+                        print(i, skill_name[0])
+                    else:
+                        try:
+                            index = int(input('削除したい技能の番号を入力してください:'))
+                            skill_name = skill_names[index][0]
+                        except:
+                            print('有効な数字を入力してください')
+                            break
+                        print(skill_name)
+                elif len(skill_names) == 1:
+                    skill_name = skill_names[0][0]
+                    print(skill_name)
+                else:
+                    print('技能が存在しません。')
+                    break
+                # skill_name に消したいものが入ってる
+                c.execute('DELETE FROM status_list WHERE skill_name = ? AND chara_name = ?', (skill_name, self.current_character))
+                #n = c.fetchone()
+                conn.commit()
 
     def do_neko(self, inp):
         '''にゃーん'''
