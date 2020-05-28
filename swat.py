b8c4bebc15fa49eef0a791ab56e9005efd66e989
t4t5u0 <ymmtryk0902@gamil.com>
2020-05-28 17:37:40 +0900

    名前変更

---
+++ swat.py
@@ -0,0 +1,9 @@
+from swlib.prompt import Command
+import pathlib
+
+def main():
+    path = pathlib.Path(__file__).resolve().parent
+    Command(path).cmdloop()
+
+if __name__ == '__main__':
+    main()
