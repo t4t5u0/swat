9dea3fbc7f7e748544cf48de6e6b3703260d2970
t4t5u0 <ymmtryk0902@gmail.com>
2020-06-04 09:38:32 +0900

    パスを修正

--- database_scripts.py
+++ database_scripts.py
@@ -1,11 +1,15 @@
 # coding: UTF-8
 
 import sys
+import pathlib
 
-from swatlib import database_commands
+from swatlib.database_commands import db_script
 
 def main():
     arg = sys.argv
+    path = pathlib.Path(__file__).resolve().parent
+    db_command = db_script(path)
+
     if '--help' in arg:
         print('データベースの作成や削除を行うことができます')
         print('パラメータにはデータベースの名前を指定することで、そのデータベースを初期化して作成することができます')
@@ -24,37 +28,37 @@ def main():
             exit(0)
         if '--all' in arg:
             # 全部消す
-            database_commands.delete_character_list()
-            database_commands.delete_status_list()
-            database_commands.delete_skill_list()
+            db_command.delete_character_list()
+            db_command.delete_status_list()
+            db_command.delete_skill_list()
             exit(0)
         if '--character_list' in arg:
             # キャラクタ一覧が入ってる
-            database_commands.delete_character_list()
+            db_command.delete_character_list()
         if '--status_list' in arg:
             # キャラクタ毎のステータスが入ってる
-            database_commands.delete_status_list()
+            db_command.delete_status_list()
         if '--skill_list' in arg:
             # ステータス変動がある技能一覧や効果の一覧が入ってる
-            database_commands.delete_skill_list()
+            db_command.delete_skill_list()
 
     # ファイル生成系
     else:
         if '--all' in arg:
             # 全部更新する
-            database_commands.create_character_list()
-            database_commands.create_status_list()
-            database_commands.create_skill_list()
+            db_command.create_character_list()
+            db_command.create_status_list()
+            db_command.create_skill_list()
             exit(0)
         if '--character_list' in arg:
             # キャラクタ一覧が入ってる
-            database_commands.create_character_list()
+            db_command.create_character_list()
         if '--status_list' in arg:
             # キャラクタ毎のステータスが入ってる
-            database_commands.create_status_list()
+            db_command.create_status_list()
         if '--skill_list' in arg:
             # ステータス変動がある技能一覧や効果の一覧が入ってる
-            database_commands.create_skill_list()
+            db_command.create_skill_list()
 
 
 if __name__ == '__main__':
